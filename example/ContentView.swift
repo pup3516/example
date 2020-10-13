5b05cd8a0851ae7e8a1c676f3e443480416ce056
pup3516 <pup3516@yandex.ru>
2020-10-13 17:41:53 +0300

    Init

---
+++ example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  example
+//
+//  Created by Mac Nalya on 13.10.2020.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Добро пожаловать!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
